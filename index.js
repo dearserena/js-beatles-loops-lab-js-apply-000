9809c669f004d58653386296ff5c18a52b82d4dd
serena Lee <dearserenalee@gmail.com>
2019-08-19 15:45:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var output = [];
+  var theBand = "";
+  
+  for (let i = 0; i < musicians.length; i++ ){
+    theBand = musicians[i] + " plays " + instruments[i]
+    output.push(theBand)
+  }
+  
+  return output
+}
+
+function johnLennonFacts(facts){  
+  var i = 0;
+  var newFacts = [];
+  
+  while (i < facts.length){
+  newFacts.push(facts[i]  + "!!!")
+   i++;
+ } 
+ 
+ return newFacts
+}
+
+function iLoveTheBeatles(10){
+  let arr = [];
+  do{
+    arr.push("I love the Beatles!"); 10++;
+  } while (10 < 15);
+} return(arr);
